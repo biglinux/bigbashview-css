cd9a6b8ebbfe52b792a0da8bfad741541fdc47c5
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2023-05-26 04:03:00 -0300

    novo e funcionando 01

---
+++ usr/share/bigbashview/bcc/js/biglinux.js
@@ -0,0 +1,9 @@
+
+#Reload
+function reload() {
+	// $("#desktop-tab-content").load(location.href+" #desktop-tab-content>*","");
+	location.href="index.sh.htm?checked_refresh=checked";
+	// alert('Algumacoisa');
+};
+
+
